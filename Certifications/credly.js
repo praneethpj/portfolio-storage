03580a3a4d9407e488aea2fd600364ed17e105e8
Praneethpj <ifeathern@gmail.com>
2025-03-28 15:09:23 -0700

    added credly

---
+++ Certifications/credly.js
@@ -0,0 +1,6 @@
+const openCredlyProfile = () => {
+  const url = "https://www.credly.com/users/herath-mudiyanselage-pubudu-praneeth-jayarathna";
+  window.open(url, "_blank"); // Opens in a new tab
+};
+
+openCredlyProfile();
